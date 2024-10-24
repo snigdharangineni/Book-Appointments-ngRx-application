8404272119f0555792a1439d1cd27c0d61d8d8ee
snigdha <sunny.rangineni@gmail.com>
2024-10-24 14:32:06 -0500

    Adjusting the reducer for effects

--- src/app/books/book.actions.ts
+++ src/app/books/book.actions.ts
@@ -2,6 +2,6 @@ import { Book } from "../models/book";
 import { createAction, props } from "@ngrx/store";
 
 export const AddBook = createAction('[Book] Add Book', props<Book>());
-
-export const AddBookSuccess = createAction('[Book] Added Book Successfully')
+export const AddBookSuccess = createAction('[Book] Added Book Successfully', props<Book>());
+export const AddBookFailure = createAction('[Book] Add Book Failure', props<{error: any}>())
 export const RemoveBook = createAction('[Book] Remove Book', props<{bookId: string}>());
--- src/app/books/book.reducer.ts
+++ src/app/books/book.reducer.ts
@@ -1,10 +1,16 @@
 import { createReducer, on } from "@ngrx/store";
 import { AddBook, RemoveBook } from "./book.actions";
 import { Book } from "../models/book";
+import { AddBookSuccess, AddBookFailure } from "./book.actions";
 
 export const initialState: Book[] = [];
 export const BookReducer = createReducer(
     initialState, 
-    on(AddBook, (state, {id, title, author}) => [...state, {id, title, author}]),
+    on(AddBook, (state, {id, title, author}) => {return state}),
+    on(AddBookSuccess, (state, {id, title, author}) => [...state, {id, title, author}]),
+    on(AddBookFailure, (state, {error}) => {
+        console.error(error);
+        return state;
+    }),
     on(RemoveBook, (state, {bookId}) => state.filter(book => book.id !== bookId))
 );
