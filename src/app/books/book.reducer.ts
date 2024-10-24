00923880edd2138c503bc4545412ebcf2fa24997
snigdha <sunny.rangineni@gmail.com>
2024-10-24 11:47:26 -0500

    Creating the store

--- src/app/books/book.reducer.ts
+++ src/app/books/book.reducer.ts
@@ -3,7 +3,7 @@ import { AddBook, RemoveBook } from "./book.actions";
 import { Book } from "../models/book";
 
 export const initialState: ReadonlyArray<Book> = [];
-export const bookreducer = createReducer(
+export const BookReducer = createReducer(
     initialState, 
     on(AddBook, (state, {id, title, author}) => [...state, {id, title, author}]),
     on(RemoveBook, (state, {bookId}) => state.filter(book => book.id !== bookId))
