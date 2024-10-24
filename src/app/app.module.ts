561faf1ac4454d4648c6b2ca1239041e71a35394
snigdha <sunny.rangineni@gmail.com>
2024-10-24 12:49:10 -0500

    creating the UI to see the reactive application in action

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BookReducer } from './books/book.reducer';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookListComponent } from './book-list/book-list.component';
+import { AppState } from './app.state';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,7 @@ import { BookListComponent } from './book-list/book-list.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({book: BookReducer})
+    StoreModule.forRoot<AppState>({book: BookReducer})
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/books/book.reducer.ts
+++ src/app/books/book.reducer.ts
@@ -2,7 +2,7 @@ import { createReducer, on } from "@ngrx/store";
 import { AddBook, RemoveBook } from "./book.actions";
 import { Book } from "../models/book";
 
-export const initialState: ReadonlyArray<Book> = [];
+export const initialState: Book[] = [];
 export const BookReducer = createReducer(
     initialState, 
     on(AddBook, (state, {id, title, author}) => [...state, {id, title, author}]),
